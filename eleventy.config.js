8f4779d4369dfdab458f2743c5d7ac6be8dda28d
Cody Rodgers <codrod63@gmail.com>
2026-03-11 23:16:12 -0400

    Fix CNAME

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
   //We use just the app.js name when in dev mode but a hashed name for cache-busting in prod
   eleventyConfig.addGlobalData("isDev", process.env.ELEVENTY_RUN_MODE === "serve");
   
-  eleventyConfig.addPassthroughCopy({"assets/*.*": "./"});
+  eleventyConfig.addPassthroughCopy({"assets/**": "./"});
   eleventyConfig.addPassthroughCopy("img");
 
 	eleventyConfig.setServerOptions({
